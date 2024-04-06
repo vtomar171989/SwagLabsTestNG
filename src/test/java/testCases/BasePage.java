7c0aa50b3f994b38de474254acffd519af8a7ccd
Jara Damessa <jaradamessa@gmail.com>
2024-04-06 10:15:53 -0500

    added chromeOptions

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -1,9 +1,13 @@
 package testCases;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,12 +15,20 @@ public class BasePage {
 	WebDriver driver;
 
 	@BeforeMethod
-	public void browserLaunch() {
+	public void browserLaunch() throws MalformedURLException {
 
 		driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/v1/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox"); 
+		options.addArguments("--disable-dev-shm-using") ;
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--headless") ;;
+		driver = new RemoteWebDriver(new URL("http://54.167.125.14"),options);
+
 
 	}
 
