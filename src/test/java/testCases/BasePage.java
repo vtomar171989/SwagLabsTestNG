a312569f672b9b2bd184502e8e3c8baf8363f0b3
jaradamessa <67838264+jaradamessa@users.noreply.github.com>
2024-04-06 14:17:42 -0500

    Update BasePage.java

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -13,7 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BasePage {
-	WebDriver driver;
+	RemoteWebDriver driver;
 
 	@BeforeMethod
 	public void browserLaunch() throws MalformedURLException {
