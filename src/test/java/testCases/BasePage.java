d613dc66345aa036398500a3ee313b62c405fb4f
Jara Damessa <jaradamessa@gmail.com>
2024-04-06 12:42:37 -0500

    added grid command

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,17 +18,34 @@ public class BasePage {
 	@BeforeMethod
 	public void browserLaunch() throws MalformedURLException {
 
-		driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/v1/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String DriverType = System.getProperty("Browser");
+		if (DriverType.contains("firefox")) {
+
+			driver = new FirefoxDriver();
+
+		} else if (DriverType.contains("remote")) {
+			
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox"); 
+			options.addArguments("--disable-dev-shm-using") ;
+			options.addArguments("--window-size=1920,1080");
+			options.addArguments("--headless") ;;
+			driver = new RemoteWebDriver(new URL("http://3.87.30.31:4444"),options);
+		}
 		
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox"); 
-		options.addArguments("--disable-dev-shm-using") ;
-		options.addArguments("--window-size=1920,1080");
-		options.addArguments("--headless") ;;
-		driver = new RemoteWebDriver(new URL("http://54.167.125.14"),options);
+		else {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-using");
+			options.addArguments("--window-size=1920,1080");
+			options.addArguments("--headless");
+			driver = new ChromeDriver(options);
+		}
+		
+
+	driver.get("https://www.saucedemo.com/v1/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 
 	}
