8ddae37be7e62acdca16a1a4c2ab6c579f93d876
jaradamessa <67838264+jaradamessa@users.noreply.github.com>
2024-04-06 11:19:25 -0500

    Update BasePage.java

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -18,19 +18,20 @@ public class BasePage {
 	public void browserLaunch() throws MalformedURLException {
 
 		driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/v1/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		
 		ChromeOptions options = new ChromeOptions();
 			options.addArguments("--no-sandbox"); 
-			//options.addArguments("--disable-dev-shm-using") ;
-			//options.addArguments("--window-size=1920,1080");
-			//options.addArguments("--headless");
+			options.addArguments("--disable-dev-shm-using") ;
+			options.addArguments("--window-size=1920,1080");
+			options.addArguments("--headless");
 			options.addArguments("--ignore-ssl-errors=yes");
 			options.addArguments("--ignore-certificate-errors");
 			driver = new ChromeDriver(options);
 
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		
 
 	}
 
