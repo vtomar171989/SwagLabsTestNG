6d60daea7e468cf21b7ec6db210f436caadaa8b7
jaradamessa <67838264+jaradamessa@users.noreply.github.com>
2024-04-06 10:30:25 -0500

    Update BasePage.java

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -23,11 +23,11 @@ public class BasePage {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox"); 
-		options.addArguments("--disable-dev-shm-using") ;
-		options.addArguments("--window-size=1920,1080");
-		options.addArguments("--headless") ;;
-		driver = new RemoteWebDriver(new URL("http://54.167.125.14"),options);
+			options.addArguments("--no-sandbox"); 
+			options.addArguments("--disable-dev-shm-using") ;
+			options.addArguments("--window-size=1920,1080");
+			options.addArguments("--headless") ;
+			driver = new ChromeDriver(options);
 
 
 	}
