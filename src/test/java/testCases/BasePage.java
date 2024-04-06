711b7d86e7ab336627d8c683a20f01cac6aef78e
jaradamessa <67838264+jaradamessa@users.noreply.github.com>
2024-04-06 13:27:05 -0500

    Update BasePage.java

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -32,7 +32,7 @@ public class BasePage {
 		  options.addArguments("--disable-dev-shm-using") ;
 		  options.addArguments("--window-size=1920,1080");
 		  options.addArguments("--headless") ;; driver = new RemoteWebDriver(new
-		  URL("http://3.87.30.31:4444"),options); }
+		  URL("http://54.167.125.14:4444"),options); }
 		  
 		  else {
 		 
