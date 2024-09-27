873a0faf440895692e35ae89781ba0fc51aee822
Vaibhav Tomar <vaibhav.tomar07@gmail.com>
2024-09-27 20:58:49 +0530

    Added Comment

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
+// Comment just added
 	WebDriver driver;
 
 	@FindBy(id = "user-name")
